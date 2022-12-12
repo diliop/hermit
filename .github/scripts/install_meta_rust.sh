02b205db7da618c9d259118c298766fdbc471893
Dimitris Iliopoulos <diliopoulos@fb.com>
2022-12-12 12:46:12 -0800

    [github-actions] testing install_meta_rust.sh - quiet wget

--- .github/scripts/install_meta_rust.sh
+++ .github/scripts/install_meta_rust.sh
@@ -6,7 +6,7 @@ DIST_NAME="${DIST_VERSION}-meta"
 DIST_TAR="rust-${DIST_NAME}.tar.gz"
 RUSTUP_VERSION="meta-${DIST_VERSION}"
 
-wget -O /tmp/"${DIST_TAR}" http://rust-dists.s3-website.us-west-1.amazonaws.com/dist/"${DIST_TAR}"
+wget -q -O /tmp/"${DIST_TAR}" http://rust-dists.s3-website.us-west-1.amazonaws.com/dist/"${DIST_TAR}"
 mkdir -p ~/.rustup/toolchains/"${RUSTUP_VERSION}"
 tar -C ~/.rustup/toolchains/"${RUSTUP_VERSION}"/ --strip-components 2 -xvf /tmp/"${DIST_TAR}" rust-1.65.0-x86_64-unknown-linux-gnu/rustc/
 find ~/.rustup/toolchains/"${RUSTUP_VERSION}"/bin/ -exec sh -c "file {} | grep 'dynamically linked' | cut -d':' -f1" \; | xargs -I {} patchelf --set-interpreter /lib64/ld-linux-x86-64.so.2 {}
