61ca44304f9f7bd65ea5bf6aa2027173c950d219
Dimitris Iliopoulos <diliopoulos@fb.com>
2022-12-18 18:23:00 -0800

    [github-actions] remove patchelf from install script

--- .github/scripts/install_meta_rust.sh
+++ .github/scripts/install_meta_rust.sh
@@ -4,7 +4,7 @@ DIST_VERSION="${1}"
 DIST_NAME="${DIST_VERSION}-meta"
 DIST_TAR="rust-${DIST_NAME}.tar.gz"
 DIST_URL="http://rust-dists.s3-website.us-west-1.amazonaws.com/dist/${DIST_TAR}"
-RUSTUP_VERSION="meta-${DIST_VERSION}"
+RUSTUP_VERSION="meta-${DIST_VERSION}" # intentionally changing from VERSION-meta to meta-VERSION to make cargo play nice
 RUSTUP_DIR="${HOME}/.rustup/toolchains/${RUSTUP_VERSION}"
 
 echo "[INFO] downloading ${DIST_NAME} dist from ${DIST_URL}"
@@ -12,11 +12,8 @@ wget -q -O /tmp/"${DIST_TAR}" "${DIST_URL}"
 
 echo "[INFO] untarring ${DIST_TAR} at ${RUSTUP_DIR}"
 mkdir -p "${RUSTUP_DIR}"
-tar -C "${RUSTUP_DIR}" --strip-components 2 -xvf /tmp/"${DIST_TAR}" rust-1.65.0-x86_64-unknown-linux-gnu/rustc/
+tar -C "${RUSTUP_DIR}" --strip-components 2 -xvf /tmp/"${DIST_TAR}" rust-"${DIST_VERSION}"-x86_64-unknown-linux-gnu/rustc/
 
-echo "[INFO] fixing binaries under ${RUSTUP_DIR}"
-find "${RUSTUP_DIR}"/bin/ -exec sh -c "file {} | grep 'dynamically linked' | cut -d':' -f1" \; | xargs -I {} patchelf --set-interpreter /lib64/ld-linux-x86-64.so.2 {}
-
-echo "[INFO] installation finished with:"
+echo "[INFO] installation for ${RUSTUP_VERSION} finished with:"
 cargo +"${RUSTUP_VERSION}" --version
 rustc +"${RUSTUP_VERSION}" --version
